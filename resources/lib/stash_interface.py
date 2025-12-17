ea85d6a924bd44d9be62fccd71374357c708c9a0
Michele Giacomoli <michele.giacomoli@gmail.com>
2025-12-17 01:56:32 +0100

    Set scene title to filename as fallback if title is empty

--- resources/lib/stash_interface.py
+++ resources/lib/stash_interface.py
@@ -66,6 +66,7 @@ query findScenes($scene_filter: SceneFilterType, $filter: FindFilterType!) {
         audio_codec
         width
         height
+        path
       }
       studio {
         name
@@ -124,6 +125,7 @@ query findScene($id: ID) {
       audio_codec
       width
       height
+      path
     }
     studio {
       name
@@ -155,6 +157,7 @@ query findScene($id: ID) {
           audio_codec
           width
           height
+          path
         }
         studio {
           name
@@ -305,6 +308,7 @@ query findSceneMarkers($markers_filter: SceneMarkerFilterType, $filter: FindFilt
           audio_codec
           width
           height
+          path
         }
         studio {
           name
